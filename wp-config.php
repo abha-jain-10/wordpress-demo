b6c248496e91bc72d809cb7c923f0ca32a554e90
abha <abha.jain@aksystems-inc.com>
2016-12-13 15:06:36 +0530

    First commit to b1 branch

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'aks@123');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost12');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
